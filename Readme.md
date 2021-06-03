8f451d8ad569720dc58cd6ab530a97618d200955
aizwellenstan <aizwellenstan@gmail.com>
2021-06-04 02:43:53 +0900

    stock screener

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Stock Screener
+https://www.tradingview.com/screener/
+
+get data from response
---
+++ main.py
@@ -0,0 +1,14 @@
+import json
+import pandas as pd
+
+with open('./screener.json') as f:
+    df = json.load(f)
+df = df['data']
+
+symbolLst = []
+for i in df:
+  symbol = i['d'][1]
+  symbolLst.append(symbol)
+
+df = pd.DataFrame(symbolLst)
+df.to_csv('./symbolLst.csv')
